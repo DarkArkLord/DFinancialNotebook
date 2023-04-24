a40bb92612b99f5c27f4eeae6e579bd15767ecff
DarkLord <dfhrhfan12@yandex.ru>
2023-04-24 23:20:56 +0400

    Add named adapter

---
+++ Android/app/src/main/java/dark/andapp/dfinnb/presentaion/adapters/NamedAdapter.kt
@@ -0,0 +1,37 @@
+package dark.andapp.dfinnb.presentaion.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import dark.andapp.dfinnb.databinding.RecyclerViewNamedListItemBinding
+import dark.andapp.dfinnb.domain.entity.NamedEntity
+
+class NamedAdapter(
+    private val entities: List<NamedEntity>
+) : RecyclerView.Adapter<NamedAdapter.ViewHolder>() {
+
+    class ViewHolder(val binding: RecyclerViewNamedListItemBinding) :
+        RecyclerView.ViewHolder(binding.root)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = RecyclerViewNamedListItemBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        with(holder.binding) {
+            val entity = entities[position]
+
+            tvId.text = entity.id.toString()
+            tvName.text = entity.name
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return entities.count()
+    }
+}
