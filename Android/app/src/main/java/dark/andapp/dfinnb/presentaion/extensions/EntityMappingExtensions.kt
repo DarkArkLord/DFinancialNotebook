1e37ba71ad5f4c7218f2f45e4497e034a99523d3
DarkLord <dfhrhfan12@yandex.ru>
2023-05-07 21:09:12 +0400

    Add category spinner, validations and creating

--- Android/app/src/main/java/dark/andapp/dfinnb/presentaion/extensions/EntityMappingExtensions.kt
+++ Android/app/src/main/java/dark/andapp/dfinnb/presentaion/extensions/EntityMappingExtensions.kt
@@ -6,6 +6,7 @@ typealias DataN = dark.andapp.dfinnb.data.local.entity.INamedEntity
 typealias DataT = dark.andapp.dfinnb.data.local.entity.TransactionEntity
 
 typealias DomainT = dark.andapp.dfinnb.domain.entity.TransactionEntity
+typealias DomainNT = dark.andapp.dfinnb.domain.entity.NullableTransactionEntity
 
 fun DataN.toDomain(): NamedEntity {
     return NamedEntity(
@@ -32,6 +33,17 @@ fun DomainT.toData(): DataT {
         categoryId = this.category.id,
         amount = this.amount,
         createdAt = this.createdAt,
+        comment = this.comment,
+    )
+}
+
+fun DomainNT.toDomain(): DomainT {
+    return DomainT(
+        id = 0,
+        bank = this.bank!!,
+        category = this.category!!,
+        amount = this.amount!!,
+        createdAt = this.createdAt!!,
         comment = this.comment,
     )
 }
--- Android/app/src/main/java/dark/andapp/dfinnb/presentaion/fragments/CreateTransactionsFragment.kt
+++ Android/app/src/main/java/dark/andapp/dfinnb/presentaion/fragments/CreateTransactionsFragment.kt
@@ -1,6 +1,7 @@
 package dark.andapp.dfinnb.presentaion.fragments
 
 import android.app.AlertDialog
+import android.content.DialogInterface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -15,6 +16,7 @@ import dagger.hilt.android.AndroidEntryPoint
 import dark.andapp.dfinnb.databinding.FragmentCreateTransactionBinding
 import dark.andapp.dfinnb.domain.entity.NullableTransactionEntity
 import dark.andapp.dfinnb.presentaion.extensions.launchWhenStarted
+import dark.andapp.dfinnb.presentaion.extensions.toDomain
 import dark.andapp.dfinnb.presentaion.viewmodels.BankAccountViewModel
 import dark.andapp.dfinnb.presentaion.viewmodels.CategoryViewModel
 import dark.andapp.dfinnb.presentaion.viewmodels.TransactionsViewModel
@@ -54,7 +56,12 @@ class CreateTransactionsFragment : Fragment(), CoroutineScope by MainScope() {
             )
 
             binding.sBank.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-                override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
                     newTransaction.bank = it[position]
                 }
 
@@ -64,36 +71,61 @@ class CreateTransactionsFragment : Fragment(), CoroutineScope by MainScope() {
             }
         }.launchWhenStarted(lifecycleScope)
 
-//        viewModel.getAll().onEach {
-//            binding.recyclerViewTransactions.adapter = TransactionAdapter(
-//                transactions = it
-//            )
-//            binding.tvCurrentBalance.text = it.sumOf { it.amount }.toString()
-//        }.launchWhenStarted(lifecycleScope)
-//
-//        binding.cvBank.setOnClickListener {
-//            val fragment = BankAccountFragment()
-//            parentFragmentManager
-//                .beginTransaction()
-//                .add(R.id.fragment_container_view, fragment)
-//                .commit()
-//        }
-//
-//        binding.cvCategory.setOnClickListener {
-//            val fragment = CategoryFragment()
-//            parentFragmentManager
-//                .beginTransaction()
-//                .add(R.id.fragment_container_view, fragment)
-//                .commit()
-//        }
-//
-//        binding.cvAdd.setOnClickListener {
-//            val fragment = AddTransaction()
-//            parentFragmentManager
-//                .beginTransaction()
-//                .add(R.id.fragment_container_view, fragment)
-//                .commit()
-//        }
+        categoryViewModel.getAll().onEach {
+            binding.sCategory.adapter = ArrayAdapter(
+                context,
+                support_simple_spinner_dropdown_item,
+                it.map { it.name }
+            )
+
+            binding.sCategory.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    newTransaction.category = it[position]
+                }
+
+                override fun onNothingSelected(p0: AdapterView<*>?) {
+                    newTransaction.category = null
+                }
+            }
+        }.launchWhenStarted(lifecycleScope)
+
+        binding.cvAdd.setOnClickListener {
+            var canCreate = true
+
+            if (newTransaction.bank == null) {
+                createDialog("Bank must be selected")
+                canCreate = false
+            }
+
+            if (newTransaction.category == null) {
+                createDialog("Category must be selected")
+                canCreate = false
+            }
+
+            if (newTransaction.amount == null) {
+                createDialog("Amount must be entered")
+                canCreate = false
+            }
+
+            if (newTransaction.createdAt == null) {
+                createDialog("Creating date must be entered")
+                canCreate = false
+            }
+
+            if (canCreate) {
+                transactionsViewModel.createTransaction(newTransaction.toDomain())
+
+                parentFragmentManager
+                    .beginTransaction()
+                    .remove(this)
+                    .commit()
+            }
+        }
 
         binding.ivArrowBack.setOnClickListener {
             parentFragmentManager
@@ -101,6 +133,14 @@ class CreateTransactionsFragment : Fragment(), CoroutineScope by MainScope() {
                 .remove(this)
                 .commit()
         }
+    }
+
+    private fun createDialog(text: String) {
+        val context = this.requireContext()
+        AlertDialog.Builder(context)
+            .setNeutralButton("Ok") { dialog, id -> dialog.cancel() }
+            .setMessage(text)
+            .show()
     }
 
     override fun onDestroyView() {
