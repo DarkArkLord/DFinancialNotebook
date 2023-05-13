ec8ca9e9a942d18954e3bc01f2ff64cffec2ab71
DarkLord <dfhrhfan12@yandex.ru>
2023-05-14 01:05:27 +0400

    Fix number flooring

--- Android/app/src/main/java/dark/andapp/dfinnb/presentaion/extensions/ViewExtensions.kt
+++ Android/app/src/main/java/dark/andapp/dfinnb/presentaion/extensions/ViewExtensions.kt
@@ -5,26 +5,33 @@ import android.content.Context
 import android.widget.TextView
 import androidx.core.content.ContextCompat
 import dark.andapp.dfinnb.R
+import java.math.RoundingMode
+import java.text.DecimalFormat
 
 fun TextView.setColoredNumberRG(value: Number) {
-    this.setTextColor(
-        ContextCompat.getColor(
-            this.context,
-            value.toDouble().let {
-                if (it == 0.0)
-                    R.color.yellow
-                else if (it < 0)
-                    R.color.red
-                else
-                    R.color.green
-            }
-        )
-    )
+    DecimalFormat("#.###").apply {
+        roundingMode = RoundingMode.DOWN
+    }.format(value).toDouble()
+        .also {
+            val color = if (it == 0.0)
+                R.color.yellow
+            else if (it < 0)
+                R.color.red
+            else
+                R.color.green
 
-    this.text = if (value.toDouble() <= 0)
-        value.toString()
-    else
-        "+${value}"
+            this.setTextColor(
+                ContextCompat.getColor(
+                    this.context,
+                    color
+                )
+            )
+
+            this.text = if (it <= 0)
+                it.toString()
+            else
+                "+${value}"
+        }
 }
 
 fun Context.createInfoDialog(text: String) {
